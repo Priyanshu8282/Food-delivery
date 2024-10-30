5becbeecf15935d1dce80b929576453527f538ea
PRIYANSHU SHARMA <140371901+Priyanshu8282@users.noreply.github.com>
2024-10-30 20:00:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,6 @@ app.use(cors());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const path = require('path');
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 
