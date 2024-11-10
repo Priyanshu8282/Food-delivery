95a19d3eeab8835b63b2d13377d7234c73910069
PRIYANSHU SHARMA <140371901+Priyanshu8282@users.noreply.github.com>
2024-11-10 15:42:14 +0530

    Update Verify.jsx

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -3,27 +3,31 @@ import './Verify.css'
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useContext } from 'react';
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 function Verify() {
     const [searchParams, setSearchParams] = useSearchParams();
     const success=searchParams.get('success');
-    const orderId=searchParams.get('success');
+    const orderId=searchParams.get('orderId');
     const {url}=useContext(StoreContext);
     const navigate=useNavigate();
-    const verifyPayment=async()=>{
-        const response=await fetch(url+'/api/order/verify',{
-            success,orderId
-        });
-        if(response.data.success){
-            navigate('/myorders');
-        }
-        else{
+    const verifyPayment = async () => {
+        try {
+            const response = await axios.get(url+"/api/order/verify", {
+                params: { success, orderId }
+            });
+            
+            if (response.data.success) {
+                navigate('/myorders');
+            } else {
+                navigate('/');
+            }
+        } catch (error) {
+            console.error("Verification failed:", error);
             navigate('/');
         }
-
-
-
-    }
+    };
+    
     useEffect(() => {
         verifyPayment();
     }, [])
